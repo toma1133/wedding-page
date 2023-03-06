9322b41b6b68c065c2b40e774879b34fe9a5bb8c
NickSu <6756536+toma1133@users.noreply.github.com>
2023-03-06 22:59:26 +0800

    Updates

---
+++ precache-manifest.748a4704fb2446dbbc9481911bd4ce73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab9b07384e4ad4cb280a",
+    "url": "/static/css/main.d4e7c56a.chunk.css"
+  },
+  {
+    "revision": "ab9b07384e4ad4cb280a",
+    "url": "/static/js/main.ab9b0738.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d056a4305b82c3cd64521747ec46e2a8",
+    "url": "/index.html"
+  }
+];
