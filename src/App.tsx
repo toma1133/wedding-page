be6c03b626cf7e725e40b117fb416d3563a050b6
NickSu <6756536+toma1133@users.noreply.github.com>
2023-03-06 23:37:41 +0800

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { GlobalStyle, ResetStyle } from './globalStyles'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ResetStyle />
+      <GlobalStyle />
+      Hello world
+    </>
+  )
 }
 
-export default App;
+export default App
